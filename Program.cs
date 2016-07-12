fa8e07e4b7d50c30c6b534d170d19cd2c4063564
Dan Ventura <danwventura@icloud.com>
2016-07-11 20:22:05 -0500

    Added loop for letters array of user's name

--- Program.cs
+++ Program.cs
@@ -12,6 +12,9 @@ namespace Cheers
             Console.WriteLine("What's your name?");
             Console.Write("> ");
             String user_name = Console.ReadLine(); //Read from terminal save to a variable
+            for(int i = 0; i < user_name.Length; i++){
+                Console.WriteLine("Give me a..." + user_name[i]);
+            }
             // user_name.ToUpper() is turning the user's name to uppercase
             //ToUpper() is functionality specific to strings
             //ToUpper() accepts no arguments
